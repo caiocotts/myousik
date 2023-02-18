17011e3e7080e623854e309497c19aa0ffdaddfe
Caio Cotts <caio@cotts.com.br>
2023-02-17 23:02:53 -0500

    Implement login page.

---
+++ js/validation.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('.error').hide()
+    $("#login-btn").click(function () {
+        let checked = true
+
+        $('.error').hide()
+        var username = $("input#username").val()
+        if (username === "") {
+            $("label#username_missing_error").show()
+            $("input#username").focus()
+            checked = false
+        }
+
+        var password = $("input#password").val()
+        if (password === "") {
+            $("label#password_missing_error").show()
+            $("input#password").focus()
+            checked = false
+        } 
+
+        if (checked) {
+            alert("First name: " + username + "\n" + "Password: " + password + "\n" )
+            
+            return true
+        }
+        return false
+    })
+})
