0c55050954577415cdccd22abbe2756150e12b87
Caio Cotts <caio@cotts.com.br>
2023-04-07 12:54:19 -0400

    Re-structure project.

--- js/change.js
+++
@@ -1,7 +0,0 @@
-let image = document.getElementById('change');
-let images = ['images/one.png','images/two.png','images/three.png','images/four.png'];
-
-setInterval(function(){
-    let random = Math.floor(Math.random()*3);
-    change.src= images[random];
-}, 5000);
---
+++ src/login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyouSIK Streaming</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <?php
+    $username = $password = $usernameErr = $passwordErr = "";
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if (empty($_POST["username"])) {
+            $usernameErr = "name field cannot be empty";
+        } else {
+            $username = $_POST["username"];
+        }
+        if (empty($_POST["password"])) {
+            $passwordErr = "password field cannot be empty";
+        } else {
+            $password = $_POST["password"];
+        }
+    }
+    ?>
+
+    <section class="header-log">
+        <nav>
+            <br>
+            <a href="index.html">
+                <img src="images/Logo2.png">
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="about.html">ABOUT</a></li>
+                        <li><a href="information.html">INFORMATION</a></li>
+                        <li><a href="feedback.html">FEEDBACK</a></li>
+                        <li><a href="login.html">LOGIN</a></li>
+                    </ul>
+                </div>
+        </nav>
+
+        <section class="login-form">
+            <div class="form-box">
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+                    <br><br>
+                    <h1>Username</h1>
+                    <div>
+                        <input type="text" name="username" id="username" value="<?php echo $username ?>">
+                        <br>
+                        <span class="error">
+                            <?php echo $usernameErr ?>
+                        </span>
+                    </div>
+                    <h1>Password</h1>
+                    <div>
+                        <input type="password" name="password" id="password" value="<?php echo $password ?>">
+                        <br>
+                        <span class="error">
+                            <?php echo $passwordErr ?>
+                        </span>
+                    </div>
+                    <a href="information.html">forgot password</a>
+                    <br><br><br>
+                    <input type="submit" id="login-btn" value="Login" />
+
+            </div>
+            </form>
+        </section>
+
+</body>
+<section class="foot">
+    <footer class="footer">
+        <p>Author: Medi Muamba, Caio Q, Aaron Jara, Chidinma Etumni</p>
+        <p>Copyright <a href="https://humber.ca/legal-and-risk-management/policies/academic/copyright-policy.html">For
+                more information click here</a></p>
+    </footer>
+</section>
+
+</html>
